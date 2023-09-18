6ca192f218831bab3266c7b8d400aac9fa78d027
kevinch123 <111613994+kevinch123@users.noreply.github.com>
2023-09-18 08:31:45 -0500

    Update Proveedor.java

--- Restaurante/src/Restaurante/Pkg/Proveedor.java
+++ Restaurante/src/Restaurante/Pkg/Proveedor.java
@@ -1,5 +1,5 @@
 package Restaurante.Pkg;
  // provedores de que se usaran en el restaurante
 public enum Proveedor {
-	COLANTA, PASTOPASTO, COLACTEOS, ROA, CARNESSABASTIAN, SURTITODO, PAPASRAFAEL,
+	COLANTA, PASTOPASTO, COLACTEOS, ROA, CARNESSABASTIAN, SURTITODO, PAPASRAFAEL,SEVILLA,
 }
