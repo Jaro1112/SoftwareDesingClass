bd83079ffaa76eca852f8d8c11abaecd556ac478
Linaviveros <142995799+Linaviveros@users.noreply.github.com>
2023-10-10 18:15:05 -0500

    Update Proveedor.java

--- Restaurante/src/Restaurante/Pkg/Proveedor.java
+++ Restaurante/src/Restaurante/Pkg/Proveedor.java
@@ -1,5 +1,7 @@
 package Restaurante.Pkg;
- // provedores de que se usaran en el restaurante
+
+ // provedores  que se usaran en el restaurante
+
 public enum Proveedor {
 	COLANTA, PASTOPASTO, COLACTEOS, ROA, CARNESSABASTIAN, SURTITODO, PAPASRAFAEL,SEVILLA,
 }
