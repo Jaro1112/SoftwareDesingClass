18974e8a5202ed055e00d4d9ed3cc95470e69373
Julian Rodriguez <111395483+Jaro1112@users.noreply.github.com>
2023-11-12 02:19:16 -0500

    Delete Restaurante directory

--- Restaurante/src/Restaurante/Pkg/Proveedor.java
+++
@@ -1,7 +0,0 @@
-package Restaurante.Pkg;
-
- // provedores  que se usaran en el restaurante
-
-public enum Proveedor {
-	COLANTA, PASTOPASTO, COLACTEOS, ROA, CARNESSABASTIAN, SURTITODO, PAPASRAFAEL,SEVILLA,
-}
